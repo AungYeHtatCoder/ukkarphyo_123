db4c0939665903d17da4a180b952ab9f2cd1ac71
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2023-12-22 17:00:12 +0630

    prize send bug fixed by amk

--- app/Jobs/ExtralEveningCheckWinner.php
+++ app/Jobs/ExtralEveningCheckWinner.php
@@ -26,36 +26,46 @@ class ExtralEveningCheckWinner implements ShouldQueue
         $this->twodWiner = $twodWiner;
     }
 
-    public function handle()
-    {
-        $today = Carbon::today();
-        $eveningSession = 'evening';
+   public function handle()
+{
+    //\Log::info('Job started for TwodWiner with prize_no: ' . $this->twodWiner->prize_no);
 
-        // Ensure this is for the evening session
-        if ($this->twodWiner->session !== $eveningSession) {
-            return;
-        }
+    $today = Carbon::today();
+    //\Log::info("Today's date: " . $today->toDateString());
 
-        // Find all winning entries for the evening session
-        $winningEntries = DB::table('lottery_two_digit_copy')
-            ->join('lotteries', 'lottery_two_digit_copy.lottery_id', '=', 'lotteries.id')
-            ->where('lottery_two_digit_copy.two_digit_id', $this->twodWiner->prize_no)
-            ->where('lottery_two_digit_copy.prize_sent', false)
-            ->whereDate('lottery_two_digit_copy.created_at', $today)
-            ->select('lottery_two_digit_copy.*')
-            ->get();
-
-        foreach ($winningEntries as $entry) {
-            DB::transaction(function () use ($entry) {
-                $lottery = Lottery::findOrFail($entry->lottery_id);
-                $user = $lottery->user;
-                $user->balance += $entry->sub_amount * 85; // Assuming the prize multiplier is 85
-                $user->save();
-
-                // Update prize_sent to true for the winning entry
-                $lottery->twoDigits()->updateExistingPivot($entry->two_digit_id, ['prize_sent' => true]);
-            });
-        }
+    if ($this->twodWiner->session !== 'evening') {
+      //  \Log::info('This TwodWiner is not for the evening session, exiting.');
+        return;
     }
+
+    // Convert prize_no to two_digit_id
+    $two_digit_id = $this->twodWiner->prize_no === '00' ? 1 : intval($this->twodWiner->prize_no, 10) + 1;
+
+    $winningEntries = DB::table('lottery_two_digit_copy')
+        ->join('lotteries', 'lottery_two_digit_copy.lottery_id', '=', 'lotteries.id')
+        ->where('lottery_two_digit_copy.two_digit_id', $two_digit_id) // Use the calculated two_digit_id
+        ->where('lottery_two_digit_copy.prize_sent', false)
+        ->whereDate('lottery_two_digit_copy.created_at', $today)
+        ->select('lottery_two_digit_copy.*')
+        ->get();
+
+    // if ($winningEntries->isEmpty()) {
+    //     \Log::info('No winning entries found.');
+    // } else {
+    //     \Log::info('Processing ' . $winningEntries->count() . ' winning entries.');
+    // }
+
+    foreach ($winningEntries as $entry) {
+        DB::transaction(function () use ($entry) {
+            $lottery = Lottery::findOrFail($entry->lottery_id);
+            $user = $lottery->user;
+            $user->balance += $entry->sub_amount * 85; // Assuming the prize multiplier is 85
+            $user->save();
+
+            // Update prize_sent to true for the winning entry
+            $lottery->twoDigits()->updateExistingPivot($entry->two_digit_id, ['prize_sent' => true]);
+        });
+    }
+}
 
 }
