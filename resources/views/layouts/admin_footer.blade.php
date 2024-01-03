d7888d35359e9a49e3db8e7d5a56ff9a67709438
AungYeHtatCoder <aungyehtatcoder2021@gmail.com>
2024-01-03 15:38:20 +0630

    footer update

--- resources/views/layouts/admin_footer.blade.php
+++ resources/views/layouts/admin_footer.blade.php
@@ -6,26 +6,13 @@
                 ©Copyright <script>
                   document.write(new Date().getFullYear())
                 </script>,
-                All Rights Reserved By <a href="https://delightmyanmar.pro/" target="__blank">Delight Myanmar</a>
-                {{-- made with <i class="fa fa-heart"></i> by
-                <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
-                for a better web. --}}
+                All Rights Reserved By <a href="#">Aladding 2D 3D</a>
+                
               </div>
             </div>
             <div class="col-lg-6">
               <ul class="nav nav-footer justify-content-center justify-content-lg-end">
-                {{-- <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">ALADDIN</a>
-                </li>
-                <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">2D</a>
-                </li>
-                <li class="nav-item">
-                  <a href="" class="nav-link text-muted" target="_blank">3D</a>
-                </li> --}}
-                {{-- <li class="nav-item">
-                  <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
-                </li> --}}
+                
               </ul>
             </div>
           </div>
